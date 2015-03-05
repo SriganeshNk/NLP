8a403fd8f613c4cc6de7f28fe66568f6fd59d3e4
srnavaneetha <sriganesh1991@gmail.com>
2015-03-05 00:25:36 -0500

    Confusion matrix

--- pos_tag.py
+++ pos_tag.py
@@ -60,7 +60,7 @@ def coarse_grain(tags):
                 tags[x][y] = (tags[x][y][0], '/' + coarse_tags[temp])
 
 
-def get_Accuracy(result, test_check):
+def get_TagMap(result, test_check):
     tag = {}
     for i in range(len(result)):
         for j in range(len(result[i])):
@@ -68,50 +68,92 @@ def get_Accuracy(result, test_check):
             r = result[i][j][1].split('/')[k]
             t = test_check[i][j][1].split('/')[1]
             if r == t:
-                if r not in tag:
+                if t not in tag:
                     other = {}
-                    other[t] = 1
-                    tag[r] = other
+                    other[r] = 1
+                    tag[t] = other
                 else:
-                    if t not in tag[r]:
-                        tag[r][t] = 1
+                    if r not in tag[t]:
+                        tag[t][r] = 1
                     else:
-                        tag[r][t] += 1
+                        tag[t][r] += 1
             else:
-                if r not in tag:
+                if t not in tag:
                     other = {}
-                    other[t] = 1
-                    tag[r] = other
+                    other[r] = 1
+                    tag[t] = other
                 else:
-                    if t not in tag[r]:
-                        tag[r][t] = 1
+                    if r not in tag[t]:
+                        tag[t][r] = 1
                     else:
-                        tag[r][t] += 1
+                        tag[t][r] += 1
     return tag
+
+
+def get_Matrix(tag):
+    tag_index = {}
+    pos_index = {}
+    i = 0
+    for x in tag.keys():
+        for y in tag[x].keys():
+            if y not in tag_index:
+                tag_index[y] = i
+                pos_index[i] = y
+                i += 1
+        if x not in tag_index:
+            tag_index[x] = i
+            pos_index[i] = x
+            i += 1
+    mat = [[0 for j in range(len(tag_index.keys()))] for i in range(len(tag_index.keys()))]
+    for i in range(len(mat)):
+        for j in range(len(mat)):
+            if pos_index[i] in tag and pos_index[j] in tag[pos_index[i]]:
+                mat[i][j] = tag[pos_index[i]][pos_index[j]]
+    return mat, pos_index
+
+
+def get_accuracy(tag, pos):
+    count = sum(tag)
+    print float(tag[pos]) / float(count)
+
+
+def display(result):
+    if isinstance(result, list):
+        for x in result:
+            print x
+        return
+    if isinstance(result, str):
+        print result
 
 
 if __name__ == '__main__':
     train, test, test_check = extract()
+    # Train using Fine-grained
     tagger = bigram_train(train)
+    # Train using Coarse-grained
     result = bigram_tag(tagger, test)
-    # checking for fine grained
-    tag = get_Accuracy(result, test_check)
-    for x in tag.keys():
-        for y in tag[x].keys():
-            print x, y, tag[x][y],
-        print
+    # Checking for fine grained
+    tag = get_TagMap(result, test_check)
+    # Get the confusion matrix
+    mat, pos_index = get_Matrix(tag)
+    # Overall Accuracy for fine grained parser
     print "Overall Accuracy:", tagger.evaluate(test_check)
+    # Convert training tags to coarse grained
     coarse_grain(train)
-    tagger = bigram_train(train)
+    # Convert test data tags to coarse grained
     coarse_grain(test_check)
+    # Convert Result tags to coarse grained
     coarse_grain(result)
     # Converting Result and test_check to coarse-grain and then checking
-    tag = get_Accuracy(result, test_check)
+    tag = get_TagMap(result, test_check)
+    # Get Confusion matrix for the checking part
+    mat, pos_index = get_Matrix(tag)
+    # train using bigram tagger for the coarse grained tags
+    tagger = bigram_train(train)
+    # test the bigram tagger using coarse tags
     result = bigram_tag(tagger, test)
-    # checking for coarse-grained
-    tag = get_Accuracy(result, test_check)
-    for x in tag.keys():
-        for y in tag[x].keys():
-            print x, y, tag[x][y],
-        print
+    # checking for coarse-grained tags
+    tag = get_TagMap(result, test_check)
+    # Get Confusion matrix for the checking part
+    mat, pos_index = get_Matrix(tag)
     print "Overall Accuracy:", tagger.evaluate(test_check)
